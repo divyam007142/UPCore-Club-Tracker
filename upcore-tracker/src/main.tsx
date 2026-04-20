c1afeef9a461aebb249c9735e99cf0aee896306b
divyam007142 <divyamsingha22@gmail.com>
2026-04-20 20:18:41 +0530

    Update main.tsx

--- upcore-tracker/src/main.tsx
+++ upcore-tracker/src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "./api/custom-fetch";
+
+// Connect frontend to backend API
+setBaseUrl("https://upcore-club-tracker.onrender.com");
+
 createRoot(document.getElementById("root")!).render(<App />);
